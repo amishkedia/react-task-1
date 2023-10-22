2e8e792745a7a426b388d71d680b9755abb27b33
amishkedia <90622227+amishkedia@users.noreply.github.com>
2023-10-22 15:12:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,116 @@
-// KindaCode.com
-// src/App.js
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ResultView from './components/ResultView';
+import Keyboard from './components/Keyboard';
+
+let output = '';
+let history = '';
+let symbols = ['*', '-', '+', '/'];
 
 function App() {
- 
-  const [text, setText] = useState('');
+	const [state, setState] = useState({
+		history: '',
+		displayValue: ''
+	});
+	const updateState = () => {
+		setState({ history: history.toString(), displayValue: output.toString() });
+	};
 
-  const [wordCount, setWordCount] = useState(0);
+	
+	const onClick = (id, keyType, value) => {
+		
+		output = output.toString();
+		
+		let lastInput = output.slice(-1);
 
+		switch (keyType) {
+			case 'function':
+				functionKey(id, lastInput);
+				break;
+			case 'operator':
+				operatorKey(value, lastInput);
+				break;
+			case 'number':
+				numberKey(value, lastInput);
+				break;
+			default:
+				return;
+		}
+	};
+	const functionKey = (id, lastInput) => {
+		const resetOutput = display => {
+			
+			history = '';
+			output = '';
+			
+			display && updateState();
+		};
+		const calculate = lastInput => {
+			
+			if (!symbols.includes(lastInput) && output) {
+				try {
+					history = output;
+					output = eval(output.replace(/%/g, '*0.01'));
+					output = Number.isInteger(output) ? output : output.toFixed(3);
+					updateState();
+					
+					history = output;
+					output = '';
+				} catch (error) {
+					output = 'Error';
+					updateState();
+					resetOutput();
+				}
+			}
+		};
 
-  const changeHandler = (event) => {
-    setText(event.target.value);
-  };
+		switch (id) {
+			case 'clear':
+				resetOutput(true);
+				break;
+			case 'clearBack':
+				output = output.slice(0, -1);
+				updateState();
+				break;
+			case 'calc':
+				calculate(lastInput);
+				break;
+			default:
+				return;
+		}
+	};
+	const operatorKey = (value, lastInput) => {
+		// PREVENT STARTING WITH AN OPERATOR
+		if (output === '' && value !== '-') {
+			return;
+		} else {
+			
+			symbols.includes(lastInput)
+				? (output = output.slice(0, -1) + value)
+				: (output += value);
+		}
+		updateState();
+	};
+	const numberKey = (value, lastInput) => {
+		
+		if (value === '.' || value === '%') {
+		
+			if (output === '' && value === '%') return;
+			lastInput === '.' || lastInput === '%' || (output += value);
+		} else {
+			output += value;
+		}
+		updateState();
+	};
 
-  useEffect(() => {
-   
-    const words = text.split(' ');
-
-    let wordCount = 0;
-    words.forEach((word) => {
-      if (word.trim() !== '') {
-        wordCount++;
-      }
-    });
-    setWordCount(wordCount);
-
-  }, [text]);
-
-  return (
-    <div className='container'>
-      <div><h1 id='h1'>Responsive Paragraph Word</h1></div>
-      <div className='row'><h1 id='h1'>Counter</h1></div>
-      <div>
-        <textarea
-          value={text}
-          onChange={changeHandler}
-          placeholder='write here'
-        ></textarea>
-
-        <div>
-          <p className='word-count'>Word Count: {wordCount}</p>
-          
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="container">
+				<ResultView history={state.history} output={state.displayValue} />
+				<Keyboard onClick={onClick} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
